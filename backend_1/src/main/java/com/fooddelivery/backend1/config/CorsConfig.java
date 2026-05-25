955676bc0bbd360c415fc12a4cd06c3383692b05
Suhani Lodhi <suhanilodhi530@gmail.com>
2026-05-26 00:37:13 +0530

    cors updated

--- backend_1/src/main/java/com/fooddelivery/backend1/config/CorsConfig.java
+++ backend_1/src/main/java/com/fooddelivery/backend1/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(List.of("http://localhost:5173", "http://localhost:5174"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:5173", "http://localhost:5174", "https://food-delivery-new-black.vercel.app"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 
